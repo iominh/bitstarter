95b65cae4e61dbff283db5eea9648a8b8bc85d6c
mtnguyen <minh.9.nguyen@gmail.com>
2013-07-07 18:22:24 -0400

    bitstarter wireframe

--- web.js
+++ web.js
@@ -1,12 +1,30 @@
 var express = require('express');
+var http = require('http'),
+    fs = require('fs');
 
 var app = express.createServer(express.logger());
+app.set("view options", {layout: false});
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.configure(function(){
+  // disable layout
+  app.set("view options", {layout: false});
+
+  // make a custom html template
+  app.register('.html', {
+    compile: function(str, options){
+      return function(locals){
+        return str;
+      };
+    }
+  });
+});
+
+app.get('/', function(req, res){
+  res.render("index.html");
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
